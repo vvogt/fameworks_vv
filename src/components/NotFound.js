142d4ccbbf8f57cbc0c7e899154ed0ac08e70d88
vvogt <vvogt@tlu.ee>
2020-03-04 12:46:39 +0200

    Remade the project folder again

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+const NotFound = () => (
+  <div>
+    <h2>Not found!</h2>
+  </div>
+);
+
+export default NotFound;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../App';
 import Main from './Main';
+import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Main} />
-      <Route path="/store/:storeId" component={App} />
-      {/* <Route component={NotFound} /> */}
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
