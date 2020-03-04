3195be79c85f6ab93e8e8856ff0abb17491a13d6
vvogt <vvogt@tlu.ee>
2020-03-04 13:44:44 +0200

    Added sass and sass-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import './scss/style.scss';
 import Router from './components/Router';
 import * as serviceWorker from './serviceWorker';
 
