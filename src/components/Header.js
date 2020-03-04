1c60b747b33816ccdc18109ed694206a34bb65b2
vvogt <vvogt@tlu.ee>
2020-03-04 23:24:29 +0200

    Finished the styling the upper part of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Login from '../assets/icons/icon_login_150x150.svg';
+import Search from '../assets/icons/icon_search_150x150.svg';
 
 class Header extends React.Component {
    render() {
-      return(
-         <div className="header">
-            <div className="header_main">
-               <div className="logo">barbican</div>
+      return (
+        <div className="header">
+          <div className="header__main">
+            <div className="logo">bardican</div>
+            <div className="header__main__right">
+              <input
+                className="search"
+                type="text"
+                style={{
+                   backgroundImage: `url(${Search})`,
+                   backgroundRepeat: `no-repeat`,
+                   backgroundPosition: `right 12px bottom 11px`,
+                   backgroundSize: `20px 20px` 
+               }}
+              />
+              <img className="search-icon" src={Search} alt="search icon" />
+              <div className="login">
+                <img className="login-icon" src={Login} alt="log in icon" />
+                <span>Log In</span>
+              </div>
             </div>
-            <div className="header_nav"></div>
-         </div>
+          </div>
+          <div className="header__nav"></div>
+        </div>
       );
    }
 }
